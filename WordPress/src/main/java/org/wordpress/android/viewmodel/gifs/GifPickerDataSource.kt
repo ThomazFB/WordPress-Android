e6f2d5eefe9ca0a5280a7b2cd4c6bca4c5868813
Thomaz Cortez <thomazfcortez@gmail.com>
2020-03-24 01:21:54 -0300

    Gif data class removed, using the MutableGifMediaViewModel instead. Added more documentation

--- WordPress/src/main/java/org/wordpress/android/viewmodel/gifs/GifPickerDataSource.kt
+++ WordPress/src/main/java/org/wordpress/android/viewmodel/gifs/GifPickerDataSource.kt
@@ -1,9 +1,9 @@
-package org.wordpress.android.viewmodel.gif
+package org.wordpress.android.viewmodel.gifs
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.paging.PositionalDataSource
-import org.wordpress.android.viewmodel.gif.provider.GifProvider
+import org.wordpress.android.viewmodel.gifs.provider.GifProvider
 
 /**
  * The PagedListDataSource that is created and managed by [GifPickerDataSourceFactory]
